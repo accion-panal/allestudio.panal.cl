dfc299d951744ecaf8be0a4b48d19c2ea2bf64c7
Fabian Salas <fabian.salas.astete@gmail.com>
2024-03-07 11:24:09 -0300

    first commit

---
+++ src/constants/consts/company.js
@@ -0,0 +1,15 @@
+export const company = {
+  statusId: 1,
+  companyId: 20128,
+  codigoUsuarioMaestro: 10075,
+};
+
+export const companyForm = {
+  id: 20128,
+};
+
+export const paginationTopLimit = {
+  limit: 9,
+  topLimit: 999999999,
+  limitPage: 1,
+};
---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+import Slide1 from '../assets/img/carousel/slide1.jpg';
+import Slide4 from '../assets/img/carousel/slide4.jpg';
+
+// import AboutMeImg from '../assets/img/me/person_4-min'
+
+/** Main Carousel Data */
+export const mainCarouselData = [
+  {
+    id: 1,
+    title: 'ALL Estudio',
+    desc: 'Nace desde la demanda de confianza en Asesorías de Corretaje y Valorización de Inmuebles',
+    img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689974575/Projects/unne/img/hero/remate3.jpg',
+  },
+  {
+    id: 2,
+    title: 'ALL Estudio',
+    desc: 'Nace desde la demanda de confianza en Asesorías de Corretaje y Valorización de Inmuebles',
+    img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689975271/Projects/unne/img/hero/financiamiento2.jpg',
+  },
+  {
+    id: 3,
+    title: 'ALL Estudio',
+    desc: 'Nace desde la demanda de confianza en Asesorías de Corretaje y Valorización de Inmuebles',
+    img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689975597/Projects/unne/img/hero/tramite2.jpg',
+  },
+  {
+    id: 4,
+    title: 'ALL Estudio',
+    desc: 'Nace desde la demanda de confianza en Asesorías de Corretaje y Valorización de Inmuebles',
+    img: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1689974937/Projects/unne/img/hero/venta.jpg',
+  },
+];
