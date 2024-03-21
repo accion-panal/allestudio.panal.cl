355b783ec05bc51d44e03ac95ff065ac88d46c5e
fabian <fabian.salas.astete@gmail.com>
2024-03-21 16:30:07 -0300

    feat: ad

--- src/constants/consts/realtor.js
+++ src/constants/consts/realtor.js
@@ -1,11 +1,11 @@
 export const realtorData = {
-  email: 'contacto@all-estudio.cl',
+  email: 'all-estudio@unne.cl',
   phone:'56983615833'
   // email: 'contacto@accionpanal.com',
 };
 
 export const footerData = {
-  email: 'contacto@all-estudio.cl',
+  email: 'all-estudio@unne.cl',
   phone:'983615833',
   wsp:'983615833',
 }
